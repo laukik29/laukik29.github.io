261409515b6a6de56f5778e198fc259c95df206a
laukik29 <laukiknakhwa@gmail.com>
2025-03-18 05:28:12 +0000

    Deploying to gh-pages from @ laukik29/laukik29.github.io@564ef0cb38247b72a9b7863af03ec0acd7a2f48d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -373,16 +373,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-started-my-undergraduate-journey-at-bits-pilani",
+          title: 'Started my Undergraduate journey at BITS, Pilani',
           description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-joined-electronics-and-robotics-club-bits-goa",
+          title: 'Joined Electronics and Robotics Club, BITS Goa',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
